95ce2fc6d6d44be9ffdd4a1f43dc273a36268b74
Louis Woodley <lwoodley@cps.edu>
2023-10-31 11:05:23 -0500

    writeup

--- a4.py
+++ a4.py
@@ -2,25 +2,82 @@
 # talking about unimplemented class attributes, don't worry about this as you're working
 
 
+
+
 class TTTBoard:
     """A tic tac toe board
+
 
     Attributes:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+
+
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+
+
+    def make_move(self, player, pos) -> bool:
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+
+
+        self.board[pos] = player
+        return True        
+
+
+    def has_won(self, player) -> bool:
+        """Check to see if the player has won"""
+        ps = [player] * 3
+        # Horizontal
+        if self.board[:3] == ps or self.board[3:6] == ps or self.board[6:] == ps:
+            return True
+        # Vertical
+        if self.board[::3] == ps or self.board[1::3] == ps or self.board[2::3] == ps:
+            return True
+        # Diagonal
+        if self.board[::4] == ps or self.board[2:7:2] == ps:
+            return True
+
+
+        return False
+
+
+    def game_over(self) -> bool:
+        """Check if the board is full or if either X or O has won"""
+        if "*" not in self.board or self.has_won("X") or self.has_won("O"):
+            return True
+
+
+        return False
+
+
+    def clear(self) -> None:
+        """Clear the board to reset game"""
+        self.board = ['*'] * 9
+
+
 
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
+
     def is_int(maybe_int: str):
         """Returns True if val is int, False otherwise
 
+
         Args:
             maybe_int - string to check if it's an int
+
 
         Returns:
             True if maybe_int is an int, False otherwise
@@ -31,21 +88,26 @@ def play_tic_tac_toe() -> None:
         except ValueError:
             return False
 
+
     brd = TTTBoard()
     players = ["X", "O"]
     turn = 0
 
+
     while not brd.game_over():
         print(brd)
         move: str = input(f"Player {players[turn]} what is your move? ")
+
 
         if not is_int(move):
             raise ValueError(
                 f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
             )
 
+
         if brd.make_move(players[turn], int(move)):
             turn = not turn
+
 
     print(f"\nGame over!\n\n{brd}")
     if brd.has_won(players[0]):
@@ -56,37 +118,62 @@ def play_tic_tac_toe() -> None:
         print(f"Board full, cat's game!")
 
 
+
+
 if __name__ == "__main__":
     # here are some tests. These are not at all exhaustive tests. You will DEFINITELY
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd.board)
+    print(brd)
     brd.make_move("X", 8)
     brd.make_move("O", 7)
+    brd.make_move("O", 8)
+    brd.make_move("X", 0)
+    print(brd)
+
 
     assert brd.game_over() == False
+
 
     brd.make_move("X", 5)
     brd.make_move("O", 6)
     brd.make_move("X", 2)
 
+
+    print(brd)
+
+
     assert brd.has_won("X") == True
     assert brd.has_won("O") == False
     assert brd.game_over() == True
 
+
     brd.clear()
 
+
+    print(brd)
+
+
     assert brd.game_over() == False
+
 
     brd.make_move("O", 3)
     brd.make_move("O", 4)
     brd.make_move("O", 5)
 
+
+    print(brd)
+
+
     assert brd.has_won("X") == False
     assert brd.has_won("O") == True
     assert brd.game_over() == True
 
+
     print("All tests passed!")
 
+
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()
--- a4_writeup.md
+++ a4_writeup.md
@@ -5,7 +5,8 @@ In assignment 4 we created a basic tic tac toe game so that we could learn objec
 ## Reflection Questions
 
 1. What was the most difficult part to tic-tac-toe?
-
+It was difficult to get the code to not check for Xs and Os that went off the side of the board. Like if there were Xs in spots 2,3, and 4, it would say three in a row, even though not all the Xs are on the same row as spot 2 is in row one and spots 3 and 4 are row two.
 2. Explain how you would add a computer player to the game.
-
+I would add a computer player by having a program use a while loop to check through the list and then use if statements to find whether or not it has an X, O, or *, and then fill the spot with whichever team it is on.
 3. If you add a computer player, explain (doesn't have to be super technical) how you might get the computer player to play the best move every time. *Note - I am not grading this for a correct answer, I just want to know your thoughts on how you might accomplish it.
+I suppose you could have the computer check what the spots on either side of it are filled with and then make a move when it finds two variables on either side that match, so it can fill in the middle with three in row. However, I understand it is probably more complicated to do than just that.
